6935e1c625baad9bf2af2750222b2626d42ddbdc
MojProfil <gluhakdaniel@gmail.com>
2022-03-29 12:47:08 +0200

    Created function generateRgb (random rgb with 3 same rgb numbers)

--- myLibrary.js
+++ myLibrary.js
@@ -2,3 +2,12 @@ function generateHexCode() {
     var random = '#'+ Math.floor(Math.random()*16777215).toString(16);
     return random;
 }
+
+var randomRgb = Math.floor(Math.random()*1000);
+function generateRgb(random) {
+    return 'rgb(' 
+    + randomRgb 
+    + ',' 
+    + randomRgb 
+    + ',' + randomRgb + ')';
+}
