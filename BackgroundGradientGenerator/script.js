e134cff8b6f90e35671776799ee0f2de3b0c9768
MojProfil <gluhakdaniel@gmail.com>
2022-03-29 12:38:11 +0200

    Created new function: generateHexCode and added to new myLibrary

--- BackgroundGradientGenerator/script.js
+++ BackgroundGradientGenerator/script.js
@@ -2,6 +2,7 @@ const body = document.querySelector('#bodyBg');
 const css = document.querySelector('h3');
 const colorOne = document.querySelector('#color1');
 const colorTwo = document.querySelector('#color2');
+const btn = document.querySelector('#randomColor');
 
 function setColor() {
     body.style.background = 'linear-gradient(to right, ' 
@@ -13,8 +14,28 @@ function setColor() {
     css.textContent = body.style.background + ';';
 }
 
+// var random = Math.floor(Math.random()*16777215).toString(16);
+
+function generateHexCode() {
+    var random = '#'+ Math.floor(Math.random()*16777215).toString(16);
+    return random;
+}
+
+function setRandomValue(random) {
+    
+    body.style.background = 'linear-gradient(to right, ' 
+    + generateHexCode()
+    + ', '
+    + generateHexCode()
+    + ')'; 
+
+    console.log(random);
+}
+
 css.textContent = body.style.background + ';';
 
-colorOne.addEventListener('input', setColor)
+colorOne.addEventListener('input', setColor);
 
-colorTwo.addEventListener('input', setColor)
+colorTwo.addEventListener('input', setColor);
+
+btn.addEventListener('click', setRandomValue);
