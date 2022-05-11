4f4a2a2563406d3bf3866e72d0fd8c6d3a8000ac
MojProfil <gluhakdaniel@gmail.com>
2022-05-11 14:03:43 +0200

    Added script.js file

---
+++ FrontendProjects/LoginForm/script.js
@@ -0,0 +1,5 @@
+const textUsername = document.querySelectorAll('username');
+const textPassword = document.querySelectorAll('password');
+const btnLogin = document.querySelectorAll('btn-login');
+const btnSignup = document.querySelectorAll('btn-signup');
+
